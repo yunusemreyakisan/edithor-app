7df582b25bb6e4ede4232ee68d33c6b9774701e7
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-10-21 17:08:34 +0300

    added splashscreen comment

--- app/src/main/java/com/app/edithormobile/MainActivity.java
+++ app/src/main/java/com/app/edithormobile/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: Splashscreen eklenecek.
+    //TODO: Add splashscreen.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
