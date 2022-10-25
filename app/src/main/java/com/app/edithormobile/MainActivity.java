08148a583867846b8d7a6ba3818a4bd8400ac1d9
onlogulcan <onl.ogulcan@gmail.com>
2022-10-25 13:50:46 +0300

    remove splash command

--- app/src/main/java/com/app/edithormobile/MainActivity.java
+++ app/src/main/java/com/app/edithormobile/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: Add splashscreen.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
