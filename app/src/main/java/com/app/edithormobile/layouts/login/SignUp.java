34158fa07acbf991853cb9fbbffe3ae254857255
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-11-02 18:03:44 +0300

    added signup sys & firebase db con.

--- app/src/main/java/com/app/edithormobile/layouts/login/SignUp.java
+++ app/src/main/java/com/app/edithormobile/layouts/login/SignUp.java
@@ -3,17 +3,35 @@ package com.app.edithormobile.layouts.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.app.edithormobile.MainActivity;
 import com.app.edithormobile.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignUp extends AppCompatActivity {
 
-    TextInputEditText ad, email, sifre;
+    FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private DatabaseReference mDatabase;
+    private HashMap<String, Object> mData;
+    TextInputEditText ad, emailadresi, sifre;
     Button kayitOl, back;
 
 
@@ -25,6 +43,7 @@ public class SignUp extends AppCompatActivity {
         //Methods
         initComponents();
         geriGonder();
+        btnKayitOlIslevi();
 
 
     }
@@ -33,7 +52,7 @@ public class SignUp extends AppCompatActivity {
     private void initComponents() {
         kayitOl = findViewById(R.id.btnKayitOl);
         ad = findViewById(R.id.txtKayitAd);
-        email = findViewById(R.id.txtKayitEmail);
+        emailadresi = findViewById(R.id.txtKayitEmail);
         sifre = findViewById(R.id.txtKayitSifre);
         back = findViewById(R.id.btnBack);
     }
@@ -46,6 +65,70 @@ public class SignUp extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
+
+    private void btnKayitOlIslevi() {
+        mAuth = FirebaseAuth.getInstance();
+        kayitOl.setOnClickListener(view -> {
+            String name = ad.getText().toString();
+            String email = emailadresi.getText().toString();
+            String password = sifre.getText().toString();
+
+            //E-Mail Validation
+            String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+            if (TextUtils.isEmpty(email) || TextUtils.isEmpty(name) || TextUtils.isEmpty(password)) {
+                Toast.makeText(SignUp.this, "Boş bırakılamaz.", Toast.LENGTH_SHORT).show();
+            } else if (password.length() < 6) {
+                Toast.makeText(SignUp.this, "Şifre 6 karakterden daha uzun olmalı.", Toast.LENGTH_SHORT).show();
+            } else if (!email.matches(emailPattern)) {
+                Log.i("E-Mail Valid", "Geçerli E-posta girildi.");
+                Toast.makeText(getApplicationContext(), "Geçersiz E-Posta", Toast.LENGTH_SHORT).show();
+            } else {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    //Veritabanına Canlı Kayıt Etme (Realtime Database)
+                                    String user_id = mAuth.getCurrentUser().getUid();
+                                    mUser = mAuth.getCurrentUser();
+                                    mDatabase = FirebaseDatabase.getInstance().getReference()
+                                            .child("Kullanicilar")
+                                            .child(user_id)
+                                            .child("Kullanıcı Bilgileri");
+
+                                    HashMap<String, String> mData = new HashMap<>();
+                                    mData.put("E-Mail", email);
+                                    mData.put("Password", password);
+                                    mData.put("Name", name);
+
+                                    //Realtime Database
+                                    mDatabase.setValue(mData).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Intent intent = new Intent(SignUp.this, MainActivity.class);
+                                                startActivity(intent);
+                                                Toast.makeText(SignUp.this, "Hesap oluşturuldu.", Toast.LENGTH_SHORT).show();
+                                            }
+                                            else {
+                                                Toast.makeText(SignUp.this, "Hesap oluşturulamadı, yeniden deneyin.", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+
+
+
+
 
 
 }
