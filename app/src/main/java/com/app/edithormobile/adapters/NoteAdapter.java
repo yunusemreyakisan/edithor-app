14fb8637bca3246bf99d597f806211fbbb60bba8
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-11-10 18:14:43 +0300

    remove note with long press

--- app/src/main/java/com/app/edithormobile/adapters/NoteAdapter.java
+++ app/src/main/java/com/app/edithormobile/adapters/NoteAdapter.java
@@ -1,23 +1,41 @@
 package com.app.edithormobile.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.edithormobile.R;
 import com.app.edithormobile.models.NoteModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
 
     Context context;
     ArrayList<NoteModel> notes;
+    DatabaseReference removeRef;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     //Constructor
     public NoteAdapter(Context context, ArrayList<NoteModel> notes) {
@@ -39,7 +57,23 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
         holder.tvTitle.setText(mNote.getNotBaslik());
         holder.tvNote.setText(mNote.getNotIcerigi());
         holder.tvOlusturmaTarihi.setText(mNote.getNotOlusturmaTarihi());
+
+        //Long press remove item
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        String user_id = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+        removeRef = (DatabaseReference) FirebaseDatabase.getInstance()
+                .getReference().child("Kullanicilar").child(user_id).child("Notlarim").child(mNote.getNoteID());
+
+        holder.itemView.setOnLongClickListener(v -> {
+            removeRef.setValue(null);
+
+
+            return false;
+        });
+
     }
+
 
     @Override
     public int getItemCount() {
