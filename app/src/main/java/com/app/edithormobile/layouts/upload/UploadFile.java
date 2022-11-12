e835a6054f14199ed27ddebc466d09bee39f82b6
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-11-12 19:12:17 +0300

    added UploadFile.java

---
+++ app/src/main/java/com/app/edithormobile/layouts/upload/UploadFile.java
@@ -0,0 +1,16 @@
+package com.app.edithormobile.layouts.upload;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.edithormobile.R;
+
+public class UploadFile extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_file);
+    }
+}
