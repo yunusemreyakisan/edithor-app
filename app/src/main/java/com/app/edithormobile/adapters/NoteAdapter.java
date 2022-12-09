246b239fd340e96dab6bdbfd6525b970c11433f1
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-12-09 10:57:17 +0300

    replaced Material to CardView

--- app/src/main/java/com/app/edithormobile/adapters/NoteAdapter.java
+++ app/src/main/java/com/app/edithormobile/adapters/NoteAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +14,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.edithormobile.R;
 import com.app.edithormobile.layouts.AddNote;
 import com.app.edithormobile.models.NoteModel;
 import com.bumptech.glide.Glide;
-import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -29,12 +30,16 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
-//TODO: iki çeşit gösterim olacak. (with images and without images)
+    //TODO: iki çeşit gösterim olacak. (with images and without images)
     Context context;
     ArrayList<NoteModel> notes;
     DatabaseReference removeRef;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
+
+    private static int TYPE_IMAGE = 1;
+    private static int TYPE_TEXT = 2;
+
 
     //Constructor
     public NoteAdapter(Context context, ArrayList<NoteModel> notes) {
@@ -42,12 +47,13 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
         this.notes = notes;
     }
 
+
     //ViewHolder with images
     public static class NoteHolder extends RecyclerView.ViewHolder {
-
         TextView tvNote, tvTitle, tvOlusturmaTarihi;
-        MaterialCardView card;
+        CardView card;
         ImageView imageUri;
+
 
         public NoteHolder(@NonNull View itemView) {
             super(itemView);
@@ -64,20 +70,61 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
     @NonNull
     @Override
     public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.activity_note_item, parent, false);
-        return new NoteHolder(v);
+       /*if (viewType == 1) {
+            return new NoteHolder(LayoutInflater.from(context).inflate(R.layout.activity_note_item, parent, false));
+        } else {
+            return new NoteHolder(LayoutInflater.from(context).inflate(R.layout.activity_note_item_without_image, parent, false));
+        }
+
+        */
+
+
+
+
+        View view = LayoutInflater.from(context).inflate(R.layout.activity_note_item, parent, false);
+        return new NoteHolder(view);
+
+
+
+
+
+       /*
+        View view = null;
+        // check here the viewType and return RecyclerView.ViewHolder based on view type
+        if (viewType == ITEM_TYPE_TWO) {
+            view = LayoutInflater.from(context).inflate(R.layout.activity_note_item, parent, false);
+            return new NoteHolder(view);
+        } else if (viewType == ITEM_TYPE_ONE) {
+            view = LayoutInflater.from(context).inflate(R.layout.button_two, parent, false);
+            return new NoteHolderWithoutImages(view);
+        }else {
+            return  null;
+        }
+
+        */
+
     }
 
+
     @Override
-    public void onBindViewHolder(@NonNull NoteAdapter.NoteHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
         NoteModel mNote = notes.get(position);
         holder.tvTitle.setText(mNote.getNotBaslik());
         holder.tvNote.setText(mNote.getNotIcerigi());
         holder.tvOlusturmaTarihi.setText(mNote.getNotOlusturmaTarihi());
-       // holder.imageUri.setImageURI(Uri.parse(mNote.getImageUri()));
-
         Bitmap bitmap = BitmapFactory.decodeFile(mNote.getImageUri());
         holder.imageUri.setImageBitmap(bitmap);
+
+/*
+        NoteHolder noteHolder = (NoteHolder) holder;
+        noteHolder.
+                noteHolder.tvNote.setText(mNote.getNotIcerigi());
+        noteHolder.tvOlusturmaTarihi.setText(mNote.getNotOlusturmaTarihi());
+
+
+        // holder.imageUri.setImageURI(Uri.parse(mNote.getImageUri()));
+
+ */
 
 
         Glide.with(context)
@@ -92,13 +139,17 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
                 .getReference().child("Kullanicilar").child(user_id).child("Notlarim").child(mNote.getNoteID());
 
         //long delete
+
         holder.card.setOnLongClickListener(v -> {
+          /*  notes.get(position).setSelected(true);
+            holder.card.setCardBackgroundColor(Color.BLUE);
+            notifyDataSetChanged();
 
-            holder.card.setChecked(!holder.card.isChecked());
+           */
 
-            // HashSet<String> dizi = new HashSet<>();
+          
 
-           if (holder.card.isChecked()) {
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 builder.setTitle("Emin misiniz?");
                 builder.setMessage("Notu silmek istediğinize emin misiniz?");
@@ -109,8 +160,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
                     notifyItemRemoved(position);
                     Toast.makeText(context, "Notunuz silindi.", Toast.LENGTH_SHORT).show();
                 });
-               builder.show();
-           }
+                builder.show();
 
 
             return true;
@@ -135,6 +185,17 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
         return notes.size();
     }
 
+
+    @Override
+    public int getItemViewType(int position) {
+        if (TextUtils.isEmpty(notes.get(position).getImageUri())) {
+            return TYPE_IMAGE;
+        } else {
+            return TYPE_TEXT;
+        }
+    }
+
+
     // method for filtering our recyclerview items.
     public void filterList(ArrayList<NoteModel> filterlist) {
         // below line is to add our filtered
@@ -144,10 +205,6 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
         // as change in recycler view data.
         notifyDataSetChanged();
     }
-
-
-
-
 
 
 }
