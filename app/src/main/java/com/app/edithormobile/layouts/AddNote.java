13963febea0ef3f784447a666cc4bf523c4dff9e
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-12-16 23:15:05 +0300

    added text recognition and detection

--- app/src/main/java/com/app/edithormobile/layouts/AddNote.java
+++ app/src/main/java/com/app/edithormobile/layouts/AddNote.java
@@ -1,7 +1,12 @@
 package com.app.edithormobile.layouts;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.net.Uri;
@@ -12,30 +17,41 @@ import android.text.TextUtils;
 import android.text.style.CharacterStyle;
 import android.text.style.StyleSpan;
 import android.text.style.UnderlineSpan;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.app.edithormobile.NotePage;
+import com.app.edithormobile.R;
 import com.app.edithormobile.databinding.ActivityAddNoteBinding;
 import com.app.edithormobile.models.NoteModel;
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 import java.io.IOException;
 import java.util.Calendar;
@@ -55,6 +71,21 @@ public class AddNote extends AppCompatActivity {
     private Uri imageUri;
     ImageView imageNote;
 
+    //Requests Code
+    static final int REQUEST_IMAGE_CODE = 100;
+    static final int STORAGE_REQUEST_CODE = 101;
+
+    //Progress Dialog
+    private ProgressDialog progressDialog;
+
+    //Text Recognizer
+    private TextRecognizer recognizer;
+
+    //Permissions
+    private String[] cameraPermission;
+    private String[] storagePermission;
+
+
     // request code
     private final int PICK_IMAGE_REQUEST = 22;
     // instance for firebase storage and StorageReference
@@ -73,16 +104,35 @@ public class AddNote extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-
+        //methods
         notKaydetmeIslevi();
         islemdenVazgec();
         optionsbarIslevi();
-        btnUploadImageIslevi();
+
+
+        //Permission
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        //progress
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Lütfen bekleyin");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        //init TextRecognizer
+        recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+        //Handle Click
+        binding.imageNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInputImageDialog();
+            }
+        });
+
 
         //share text
         binding.btnUploadImage.setOnClickListener(v -> shareNotes());
-        
-
 
         // get the Firebase  storage reference
         storage = FirebaseStorage.getInstance();
@@ -100,6 +150,8 @@ public class AddNote extends AppCompatActivity {
         styleNormal = new StyleSpan(Typeface.NORMAL);
         styleItalic = new StyleSpan(Typeface.ITALIC);
         underLine = new UnderlineSpan();
+
+        //TODO: imageNote özelinde uzun basıldığında resmi önizleme özelliği olmalı.
 
     }
     
@@ -181,25 +233,191 @@ public class AddNote extends AppCompatActivity {
     }
 
 
-    //resim yukle
-    private void btnUploadImageIslevi() {
-        binding.imageNote.setOnClickListener(this::SelectImage);
+    //recognize text
+    private void recognizeTextFromImage() {
+        progressDialog.setMessage("Resim hazırlanıyor...");
+        progressDialog.show();
+
+        if(imageUri == null){
+            Toast.makeText(AddNote.this, "Lütfen resim seçiniz", Toast.LENGTH_SHORT).show();
+        }else{
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Not bulundu");
+            builder.setMessage("Seçtiğiniz fotoğraf üzerinde not bulundu, eklemek ister misiniz?");
+            builder.setNegativeButton("Hayır", (dialog, which) ->   progressDialog.dismiss());
+            builder.setPositiveButton("Evet", (dialogInterface, i) -> {
+                try {
+                    InputImage inputImage = InputImage.fromFilePath(this, imageUri);
+                    progressDialog.setMessage("Resim çözülüyor...");
+
+                    Task<Text> textTaskResult = recognizer.process(inputImage).addOnSuccessListener(text -> {
+                        progressDialog.dismiss();
+
+                        String recognized = text.getText();
+                        binding.txtNote.setText(recognized);
+                    }).addOnFailureListener(e -> {
+                        progressDialog.dismiss();
+                        Toast.makeText(AddNote.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    });
+                }catch (IOException e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(this, "Failed prepairing image.."+ e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            alertDialog.getButton(alertDialog.BUTTON_NEGATIVE).setTextColor(getColor(R.color.button_active_color));
+            alertDialog.getButton(alertDialog.BUTTON_POSITIVE).setTextColor(getColor(R.color.button_active_color));
+
+
+
+        }
+
+
     }
 
+    //Popup Menu ile seceneklerin sorulması
+    private void showInputImageDialog() {
+        PopupMenu popupMenu = new PopupMenu(this, binding.imageNote);
 
-    //resim sec
-    private void SelectImage(View view) {
-        //Defining Implicit Intent to mobile gallery
-        Intent intent = new Intent();
+        popupMenu.getMenu().add(Menu.NONE, 1, 1, "Fotoğraf çek");
+        popupMenu.getMenu().add(Menu.NONE, 2,2, "Galeriden seç");
+
+        popupMenu.show();
+
+        //listener
+        popupMenu.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId();
+
+            if(id ==1 ){
+                if(checkCameraPermission()){
+                    pickImageCamera();
+                }else {
+                    requestCameraPermission();
+                }
+            }else if(id == 2){
+                if(checkStoragePermission()){
+                    pickImageGallery();
+                }else{
+                    requestStoragePermission();
+                }
+            }
+            return false;
+        });
+
+    }
+
+    //Galeriden fotograf secmek
+    private void pickImageGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(
-                Intent.createChooser(
-                        intent,
-                        "Select Image from here..."),
-                PICK_IMAGE_REQUEST);
-       uploadImage();
+        galleryActivityResultLauncher.launch(intent);
     }
+
+    private final ActivityResultLauncher<Intent> galleryActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                imageUri = result.getData().getData();
+                //set img view
+                binding.imageNote.setImageURI(imageUri);
+                recognizeTextFromImage();
+            } else {
+                Toast.makeText(AddNote.this, "Cancelled...", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+
+    //Kameradan fotograf cekimi
+    private void pickImageCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "Ornek baslik");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Ornek aciklama");
+
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        cameraActivityResultLauncher.launch(intent);
+    }
+
+    private final ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                binding.imageNote.setImageURI(imageUri);
+                recognizeTextFromImage();
+            } else {
+                Toast.makeText(AddNote.this, "Cancelled...", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+
+    //Galeri Erisim İzni Kontrolü
+    private boolean checkStoragePermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+    }
+
+    //Galeri Erisim İzni İsteği
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this, storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    //Kamera Erisim İzni Kontrolü
+    private boolean checkCameraPermission() {
+        boolean cameraResult = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        boolean storageResult = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+        return cameraResult == storageResult;
+    }
+
+    //Kamera Erisim İzni İsteği
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, cameraPermission, REQUEST_IMAGE_CODE);
+    }
+
+
+    //İzin istegi sonucunun kontrolu
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_IMAGE_CODE: {
+
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (cameraAccepted && storageAccepted) {
+                        pickImageCamera();
+                    } else {
+                        Toast.makeText(this, "Kamera ve Depolama izni gerekli.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            }
+            case STORAGE_REQUEST_CODE: {
+
+                if (grantResults.length > 0) {
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                    if (storageAccepted) {
+                        pickImageGallery();
+                    } else {
+                        Toast.makeText(this, "Depolama izni gerekli", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        }
+
+    }
+
+
 
     // Override onActivityResult method
     @Override
@@ -240,13 +458,6 @@ public class AddNote extends AppCompatActivity {
     // UploadImage method
     private void uploadImage() {
         if (imageUri != null) {
-
-            // Code for showing progressDialog while uploading
-            ProgressDialog progressDialog
-                    = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
             // Defining the child of storageReference
             StorageReference ref
                     = storageReference
@@ -258,53 +469,9 @@ public class AddNote extends AppCompatActivity {
             // or failure of image
             ref.putFile(imageUri)
                     .addOnSuccessListener(
-                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            taskSnapshot -> Toast.makeText(AddNote.this, "Fotograf yukleme basarılı", Toast.LENGTH_SHORT).show())
 
-                                @Override
-                                public void onSuccess(
-                                        UploadTask.TaskSnapshot taskSnapshot) {
-
-                                    // Image uploaded successfully
-                                    // Dismiss dialog
-                                    progressDialog.dismiss();
-                                    Toast
-                                            .makeText(AddNote.this,
-                                                    "Fotograf yüklendi",
-                                                    Toast.LENGTH_SHORT)
-                                            .show();
-                                }
-                            })
-
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-
-                            // Error, Image not uploaded
-                            progressDialog.dismiss();
-                            Toast
-                                    .makeText(AddNote.this,
-                                            "Failed " + e.getMessage(),
-                                            Toast.LENGTH_SHORT)
-                                    .show();
-                        }
-                    })
-                    .addOnProgressListener(
-                            new OnProgressListener<UploadTask.TaskSnapshot>() {
-
-                                // Progress Listener for loading
-                                // percentage on the dialog box
-                                @Override
-                                public void onProgress(
-                                        UploadTask.TaskSnapshot taskSnapshot) {
-                                    double progress
-                                            = (100.0
-                                            * taskSnapshot.getBytesTransferred()
-                                            / taskSnapshot.getTotalByteCount());
-                                    progressDialog.setMessage(
-                                            "Uploaded "
-                                                    + (int) progress + "%");
-                                }
-                            });
+                    .addOnFailureListener(e -> Toast.makeText(AddNote.this, "Failed", Toast.LENGTH_SHORT).show());
         }
     }
 
