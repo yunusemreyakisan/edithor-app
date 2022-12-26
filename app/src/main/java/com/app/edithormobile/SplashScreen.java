36991f0d7c8e905b9db25dcab4273f591bb5bd0d
onlogulcan <onl.ogulcan@gmail.com>
2022-12-26 21:27:15 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/app/edithormobile/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.app.edithormobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.edithormobile.layouts.login.SignIn;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent =new Intent(this, SignIn.class);
+        startActivity(intent);
+        finish();
+    }
+}
