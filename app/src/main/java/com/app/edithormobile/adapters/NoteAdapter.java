8932f6f16e965b6bd1b86826fd3d8c4ff20a1dbd
yunusemreyakisan <yunusemreyakisan@outlook.com>
2023-01-04 17:41:23 +0300

    added click listener interface

--- app/src/main/java/com/app/edithormobile/adapters/NoteAdapter.java
+++ app/src/main/java/com/app/edithormobile/adapters/NoteAdapter.java
@@ -3,15 +3,11 @@ package com.app.edithormobile.adapters;
 import static java.util.Objects.requireNonNull;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -20,56 +16,34 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.app.edithormobile.R;
 import com.app.edithormobile.models.NoteModel;
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.orhanobut.dialogplus.DialogPlus;
-import com.orhanobut.dialogplus.ViewHolder;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
-    //TODO: iki çeşit gösterim olacak. (with images and without images)
     Context context;
     ArrayList<NoteModel> notes;
     DatabaseReference removeRef;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
 
+    private static ClickListener clickListener;
 
     private static int TYPE_IMAGE = 1;
     private static int TYPE_TEXT = 2;
 
 
     //Constructor
-    public NoteAdapter(Context context, ArrayList<NoteModel> notes) {
+    public NoteAdapter(Context context, ArrayList<NoteModel> notes, ClickListener clickListener) {
         this.context = context;
         this.notes = notes;
-
+        this.clickListener = clickListener;
     }
 
 
-
-    //ViewHolder with images
-    public static class NoteHolder extends RecyclerView.ViewHolder {
-        TextView tvNote, tvTitle, tvOlusturmaTarihi;
-        CardView card;
-        ImageView imageUri;
-
-        public NoteHolder(@NonNull View itemView) {
-            super(itemView);
-            card = itemView.findViewById(R.id.card);
-            tvNote = itemView.findViewById(R.id.tvNote);
-            tvTitle = itemView.findViewById(R.id.tvTitle);
-            tvOlusturmaTarihi = itemView.findViewById(R.id.tvOlusturmaTarihi);
-            imageUri = itemView.findViewById(R.id.imageUri);
-        }
-    }
 
     @NonNull
     @Override
@@ -95,6 +69,10 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
             Glide.with(context)
                     .load(mNote.getImageUri())
                     .into(holder.imageUri);
+
+            //TODO: Fotoğrafın referansı cihazın kendi depolama alanıyla sınırlı. Storage üzerinden URL alıp göstermeli.
+
+
         } else {
             holder.tvTitle.setText(mNote.getNotBaslik());
             holder.tvNote.setText(mNote.getNotIcerigi());
@@ -108,6 +86,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
         removeRef = FirebaseDatabase.getInstance()
                 .getReference().child("Kullanicilar").child(user_id).child("Notlarim");
 
+        /*
         //Card Update
         holder.card.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -149,7 +128,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
                                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                                     @Override
                                     public void onComplete(@NonNull Task<Void> task) {
-                                        if(task.isSuccessful()){
+                                        if (task.isSuccessful()) {
                                             notes.get(holder.getLayoutPosition()).setNotBaslik(String.valueOf(baslik.getText()));
                                             notes.get(holder.getLayoutPosition()).setNotIcerigi(String.valueOf(icerik.getText()));
                                             notifyItemChanged(holder.getLayoutPosition());
@@ -170,58 +149,58 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
             }
         });
 
+         */
+    }
 
-        //Delete Data from Realtime DB
-        holder.card.setOnLongClickListener(v1 -> {
-            String id = notes.get(position).getNoteID();
-            //Toast.makeText(MainActivity.this, "Back pressed", Toast.LENGTH_SHORT).show();
-            androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(context);
-            // Pencere Baslik Tanımı
-            builder.setTitle("Emin misiniz?");
-            // Pencere Mesaj Tanımı
-            builder.setMessage("Notu silmek istediğinize emin misiniz?");
 
-            class AlertDialogClickListener implements DialogInterface.OnClickListener {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if (which == androidx.appcompat.app.AlertDialog.BUTTON_NEGATIVE) {
-                        Toast.makeText(context, "İşlem iptal edildi.",
-                                Toast.LENGTH_SHORT).show();
-                    } else if (which == androidx.appcompat.app.AlertDialog.BUTTON_POSITIVE) { // veya else
-                        removeRef.child(id).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    notes.remove(notes.get(holder.getLayoutPosition()));
-                                    notifyItemRemoved(holder.getLayoutPosition());
-                                    notifyDataSetChanged();
-                                    Toast.makeText(context, "Notunuz silindi.", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(context, "Hata olustu", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                }
+
+    //ViewHolder with images
+    public static class NoteHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
+        TextView tvNote, tvTitle, tvOlusturmaTarihi;
+        public CardView card;
+        ImageView imageUri;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            card = itemView.findViewById(R.id.card);
+            tvNote = itemView.findViewById(R.id.tvNote);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOlusturmaTarihi = itemView.findViewById(R.id.tvOlusturmaTarihi);
+            imageUri = itemView.findViewById(R.id.imageUri);
+
+            //click
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            v = card;
+            if (position >= 0) {
+                clickListener.onItemClick(v, position);
             }
+        }
 
-            // AlertDialog Builder
-            AlertDialogClickListener alertDialogClickListener = new AlertDialogClickListener();
-            builder.setPositiveButton("EVET", alertDialogClickListener);
-            builder.setNegativeButton("HAYIR", alertDialogClickListener);
-            androidx.appcompat.app.AlertDialog alertDialog = builder.create();
-            alertDialog.show();
-            alertDialog.getButton(alertDialog.BUTTON_NEGATIVE).setTextColor(context.getResources().getColor(R.color.button_active_color));
-            alertDialog.getButton(alertDialog.BUTTON_POSITIVE).setTextColor(context.getResources().getColor(R.color.button_active_color));
-            alertDialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_bg);
+        @Override
+        public boolean onLongClick(View v) {
+            int position = getAdapterPosition();
+            v = card;
+            if (position >= 0) {
+                clickListener.onItemLongClick(v, position);
+                return true;
+            }
+            return false;
+        }
+    }
 
-            return true;
-        });
 
 
+    //Interface
+    public interface ClickListener {
+        void onItemClick(View v, int position);
+        void onItemLongClick(View v, int position);
     }
 
 
@@ -246,7 +225,6 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
         notes = filterlist;
         notifyDataSetChanged();
     }
-
 
 
 }
