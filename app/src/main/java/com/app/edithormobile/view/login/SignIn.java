be6ae11fef8277a948fbb042d68b04ffa9a6323c
yunusemreyakisan <yunusemreyakisan@outlook.com>
2023-03-16 00:19:05 +0300

    removed google sign-in & updated viewmodel

--- app/src/main/java/com/app/edithormobile/view/login/SignIn.java
+++ app/src/main/java/com/app/edithormobile/view/login/SignIn.java
@@ -2,38 +2,23 @@ package com.app.edithormobile.view.login;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.app.edithormobile.R;
 import com.app.edithormobile.databinding.ActivitySignInBinding;
-import com.app.edithormobile.model.UserModel;
 import com.app.edithormobile.util.IToast;
 import com.app.edithormobile.util.Util;
 import com.app.edithormobile.view.NotePage;
-import com.app.edithormobile.viewmodel.sign_in.SignInViewModel;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.app.edithormobile.viewmodel.login.SignInViewModel;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
-import java.util.Objects;
 
 
 public class SignIn extends AppCompatActivity implements IToast {
@@ -47,119 +32,28 @@ public class SignIn extends AppCompatActivity implements IToast {
     Util util = new Util();
     SignInViewModel viewModel;
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
         super.onCreate(savedInstanceState);
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         View v = binding.getRoot();
         setContentView(v);
-
-        //ViewModel Bağlama
+        //ViewModel Bind
         viewModel = ViewModelProviders.of(this).get(SignInViewModel.class);
-
 
         //Methods
         kayitOlGonder();
         girisIslemi();
         rememberUser();
-
-
-        //configure google sign in
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        //Click to sign in button
-        binding.btnGoogle.setOnClickListener(v1 -> {
-            switch (v1.getId()) {
-                case R.id.btnGoogle:
-                    signIn();
-                    break;
-            }
-        });
     }
     //eof onCreate()
-
-
-    //TODO: Google sign in sıkıntılı,düzenlenmesi gerekiyor
-    //Google Sign-in
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    //Google ile giris isleminin sonucunun elde edilmesi
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Toast("Giris başarılı");
-            googleHesabiniKaydet(account);
-
-            Intent intent = new Intent(this, NotePage.class);
-            startActivity(intent);
-        } catch (ApiException e) {
-            Log.e("Error", "SignInResult:Failed Code = " + e.getStatusCode() + "error" + e.getMessage());
-        }
-    }
-
-    //Google ile giris yapildiginda kullanici bilgilerini Realtime DB uzerine kaydedilmesi
-    private void googleHesabiniKaydet(GoogleSignInAccount account) {
-        mAuth = FirebaseAuth.getInstance();
-
-        String name = Objects.requireNonNull(account.getGivenName());
-        String email = Objects.requireNonNull(account.getEmail());
-        String password = Objects.requireNonNull(account.getId());
-
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    //Veritabanına Canlı Kayıt Etme (Realtime Database)
-                    String user_id = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
-                    mUser = mAuth.getCurrentUser();
-                    mDatabase = FirebaseDatabase.getInstance().getReference().child("Kullanicilar").child(user_id).child("Kullanıcı Bilgileri");
-
-                    String hesapOlusturmaTarihi = util.olusturmaZamaniGetir();
-
-                    HashMap<String, UserModel> mData = new HashMap<>();
-                    mData.put(user_id, new UserModel(user_id, email, password, hesapOlusturmaTarihi));
-
-                    //Realtime Database
-                    mDatabase.setValue(mData).addOnCompleteListener(task1 -> {
-                        if (task1.isSuccessful()) {
-                            Intent intent = new Intent(SignIn.this, NotePage.class);
-                            startActivity(intent);
-                            Toast("Hesap oluşturuldu");
-                        } else {
-                            Toast("Hesap oluşturulamadı, yeniden deneyin");
-                        }
-                    });
-                }
-            }
-        });
-    }
 
     //onStart() methodu ile kullanicinin onceden giris yapıp yapmadiginin kontrolu
     @Override
     protected void onStart() {
         super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null) {
-            //Toast.makeText(this, "Kullanıcı zaten giriş yaptı", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(getApplicationContext(), NotePage.class);
-            startActivity(intent);
-        }
+
     }
 
     //Back-pressed methodu
---
+++ app/src/main/java/com/app/edithormobile/viewmodel/login/SignInViewModel.java
@@ -0,0 +1,89 @@
+package com.app.edithormobile.viewmodel.login;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.lifecycle.ViewModel;
+
+import com.app.edithormobile.databinding.ActivitySignInBinding;
+import com.app.edithormobile.view.NotePage;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class SignInViewModel extends ViewModel {
+    private final String successMessage = "Giriş başarılı";
+    private final String errorMessage = "Email veya şifre hatalı";
+
+    //Toast message
+    public void setToastMessage(Application app, String toastMessage) {
+        Toast.makeText(app, toastMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    //Kayıtlı kullanıcı girisi
+    public void kayitliKullaniciGirisi(Application app, ActivitySignInBinding binding) {
+        //Kaydolunan şifre ve ad soyad şartlanacak.
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        // Firebase üzerinden email ve şifre alınması
+        String email, password;
+        email = Objects.requireNonNull(binding.txtGrsEmail.getText()).toString();
+        password = Objects.requireNonNull(binding.txtGrsSifre.getText()).toString();
+
+        // Email ve Şifre Giriş Kontrolü (Dolu-Boş)
+        if (TextUtils.isEmpty(email)) {
+            setToastMessage(app, "Lütfen emailinizi giriniz");
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            setToastMessage(app, "Lütfen şifrenizi giriniz");
+        }
+
+        binding.pBarGiris.setVisibility(View.VISIBLE);
+        // Kayıtlı Kullanıcı Girişi
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                binding.pBarGiris.setVisibility(View.GONE);
+                // Eğer giriş bilgileri doğruysa:
+                // Anasayfaya geç.
+                Intent intent = new Intent(app, NotePage.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                app.startActivity(intent);
+                //girisSonuc = true;
+                setToastMessage(app, successMessage);
+            } else {
+                binding.pBarGiris.setVisibility(View.GONE);
+                // Giriş hatalı ise:
+                setToastMessage(app, errorMessage);
+
+                // girisSonuc = false;
+            }
+        });
+    }
+
+    //Shared Preferences (Beni Hatırla)
+    public Boolean getSharedPreferences(Application app) {
+        Boolean sonuc = null;
+        SharedPreferences preferences = app.getSharedPreferences("checkbox", MODE_PRIVATE);
+        String checkbox = preferences.getString("Remember", "");
+        //Checkbox true ise sonuc true
+        sonuc = checkbox.equals("true");
+        return sonuc;
+    }
+
+    public String setSharedPreference(Application app, String preference) {
+        SharedPreferences preferences = app.getSharedPreferences("checkbox", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Remember", preference);
+        editor.apply();
+        //Seçim degerini dondur.
+        return preference;
+    }
+
+
+}
